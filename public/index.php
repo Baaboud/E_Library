9eb0c1c6996c7d86c72b090757b67817f325b6ed
Baaboud <Baaboud.dev@gmail.com>
2022-04-03 08:18:52 +0300

    publisher branch

--- public/index.php
+++ public/index.php
@@ -22,28 +22,12 @@ $config=array(
 
 );
 $system=new AppSystem($config);
-
-/** Home routes */
-Router::get('/', [HomeController::class, 'index']);
-Router::get('/details', [HomeController::class, 'details']);
-Router::get('/checkout', [HomeController::class, 'checkout']);
-Router::get('/category', [HomeController::class, 'category']);
-Router::get('/dashboard', [HomeController::class, 'dash']);
-
 /** web routes  */
-Router::get('/categories',[CategoriesController::class,'listAll']);
-Router::get('/add_category',[CategoriesController::class,'create']);
-Router::get('/edit_category/{id}',[CategoriesController::class,'edit']);
-Router::get('/remove_category/{id}/{name}',[CategoriesController::class,'remove']);
-Router::post('/save_category',[CategoriesController::class,'store']);
-Router::post('/update_category',[CategoriesController::class,'update']);
-
-/** offer routes  */
-Router::get('/offers',[OffersController::class,'listAll']);
-Router::get('/add_offer',[OffersController::class,'create']);
-Router::get('/edit_offer/{id}',[OffersController::class,'edit']);
-Router::get('/remove_offer/{id}/{name}',[OffersController::class,'remove']);
-Router::post('/save_offer',[OffersController::class,'store']);
-Router::post('/update_offer',[OffersController::class,'update']);
+Router::get('/',[PublishersController::class,'listAll']);
+Router::get('/add_publisher',[PublishersController::class,'create']);
+Router::get('/edit_publisher/{id}',[PublishersController::class,'edit']);
+Router::get('/remove_publisher/{id}/{name}',[PublishersController::class,'remove']);
+Router::post('/save_publisher',[PublishersController::class,'store']);
+Router::post('/update_publisher',[PublishersController::class,'update']);
 
 $system->start();
