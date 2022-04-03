d704ccb889d38afff32e1489211215a3d8a9e530
Baaboud <Baaboud.dev@gmail.com>
2022-04-03 08:32:25 +0300

    cities branch

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use coding\app\controllers\AuthorsController;
 use coding\app\controllers\CategoriesController;
+use coding\app\controllers\CitiesController;
 use coding\app\controllers\HomeController;
 use coding\app\controllers\OffersController;
 use coding\app\controllers\PublishersController;
@@ -23,27 +24,12 @@ $config=array(
 );
 $system=new AppSystem($config);
 
-/** Home routes */
-Router::get('/', [HomeController::class, 'index']);
-Router::get('/details', [HomeController::class, 'details']);
-Router::get('/checkout', [HomeController::class, 'checkout']);
-Router::get('/category', [HomeController::class, 'category']);
-Router::get('/dashboard', [HomeController::class, 'dash']);
-
 /** web routes  */
-Router::get('/categories',[CategoriesController::class,'listAll']);
-Router::get('/add_category',[CategoriesController::class,'create']);
-Router::get('/edit_category/{id}',[CategoriesController::class,'edit']);
-Router::get('/remove_category/{id}/{name}',[CategoriesController::class,'remove']);
-Router::post('/save_category',[CategoriesController::class,'store']);
-Router::post('/update_category',[CategoriesController::class,'update']);
-
-/** offer routes  */
-Router::get('/offers',[OffersController::class,'listAll']);
-Router::get('/add_offer',[OffersController::class,'create']);
-Router::get('/edit_offer/{id}',[OffersController::class,'edit']);
-Router::get('/remove_offer/{id}/{name}',[OffersController::class,'remove']);
-Router::post('/save_offer',[OffersController::class,'store']);
-Router::post('/update_offer',[OffersController::class,'update']);
+Router::get('/',[CitiesController::class,'listAll']);
+Router::get('/add_city',[CitiesController::class,'create']);
+Router::get('/edit_city/{id}',[CitiesController::class,'edit']);
+Router::get('/remove_city/{id}/{name}',[CitiesController::class,'remove']);
+Router::post('/save_city',[CitiesController::class,'store']);
+Router::post('/update_city',[CitiesController::class,'update']);
 
 $system->start();
